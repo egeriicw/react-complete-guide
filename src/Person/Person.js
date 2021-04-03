91ac71564dc81ac8fba2a2eb06fcc23d122051ad
Bill Eger <egeriicw@gmail.com>
2021-04-03 15:52:20 -0400

    Outputting Dynamic Content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const person = () => {
     // ES6 Arrow function syntax
 
-    return <p>I'm a Person!</p>
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 
 }
 
