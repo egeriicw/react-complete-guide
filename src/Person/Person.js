d4a65011361a4fd9c245a0dd67e8a8c966d66a4e
Bill Eger <egeriicw@gmail.com>
2021-04-03 15:56:35 -0400

    Working with Props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const person = () => {
+const person = (props) => {
     // ES6 Arrow function syntax
 
-    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    return <p>I'm {props.name} and I am {props.age} years old!</p>
 
 }
 
