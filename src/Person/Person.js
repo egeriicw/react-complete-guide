859d70df699596d4ed38f10eebb1650fc4ad14b1
Bill Eger <egeriicw@gmail.com>
2021-04-04 17:12:08 -0400

    Adding Styling with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     // ES6 Arrow function syntax
 
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
